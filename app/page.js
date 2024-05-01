f05cd642f916be5f191b5136363970ef465fdc16
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-05-01 18:07:57 -0400

    Add nav container

--- app/page.js
+++ app/page.js
@@ -1,3 +1,21 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <nav className="px-48 py-4 flex justify-between w-full">
+        <Link href="/">
+          <Image
+            src="/wordmark.png"
+            alt="Wordmark"
+            height={500}
+            width={500}
+            className="w-32"
+          />
+        </Link>
+        <div className="flex gap-10 items-center"></div>
+      </nav>
+    </main>
+  );
 }
