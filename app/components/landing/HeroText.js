363f62e969ecc028ac6fa6fb84152976a56fa4de
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-05-02 00:20:21 -0400

    Adjust gradient

--- app/components/landing/HeroText.js
+++ app/components/landing/HeroText.js
@@ -30,7 +30,7 @@ export default function HeroText() {
     <div className="flex justify-center text-center mt-10">
       <div>
         <motion.button
-          className="rounded-full cursor-help px-3 py-1 bg-gradient-to-r font-normal text-sm from-blue-400 to-blue-500 text-white"
+          className="rounded-full cursor-help px-3 py-1 bg-gradient-to-r font-normal text-sm from-blue-500 to-blue-600 text-white"
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.2, type: "spring", stiffness: 100 }}
@@ -38,7 +38,7 @@ export default function HeroText() {
           Private beta
         </motion.button>
 
-        <div className="text-6xl tracking-tighter mt-4">
+        <div className="text-5xl tracking-tighter mt-4">
           <div>
             {firstWords.map((word, index) => (
               <motion.span
