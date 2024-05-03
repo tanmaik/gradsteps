4ca3e313fba4cbe54a3152cc1166a0f5c93d4b26
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-05-03 11:11:19 -0400

    Finish student header

--- app/components/landing/StudentsHeader.js
+++ app/components/landing/StudentsHeader.js
@@ -3,23 +3,35 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 export default function StudentsHeader() {
   return (
-    <motion.div
-      className="text-center mt-16 font-medium"
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true }}
-      transition={{
-        delay: 0.2,
-        duration: 0.3,
-        type: "spring",
-        stiffness: 40,
-      }}
-    >
-      <p className="text-sm">Students</p>
-      <h1 className="text-4xl tracking-tight mt-2">
-        Generate a course plan in seconds.
-      </h1>
-      <div className="flex justify-center mt-6">
+    <div className="text-center mt-16 font-medium">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{
+          delay: 0.2,
+          duration: 0.2,
+          type: "spring",
+          stiffness: 40,
+        }}
+      >
+        <p className="text-sm">Students</p>
+        <h1 className="text-4xl tracking-tight mt-2">
+          Generate a course plan in seconds.
+        </h1>
+      </motion.div>
+      <motion.div
+        className="flex justify-center mt-6"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{
+          delay: 0.4,
+          duration: 0.2,
+          type: "spring",
+          stiffness: 40,
+        }}
+      >
         <Image
           src="/example-input.png"
           width={5740}
@@ -27,7 +39,7 @@ export default function StudentsHeader() {
           alt="Example input"
           className="w-[80%]"
         />
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 }
